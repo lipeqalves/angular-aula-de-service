aad91d1dde43034894a215b8e40f3b226bad8074
Filipe Alves <lipeqa@gmail.com>
2023-04-23 09:38:02 -0300

    Desacoplando o componente e deixando-o inteligente

--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./card.component.css']
 })
 export class CardComponent {
-
+name:string = "dito"
+attributesTypes:string[]=['fire', 'rock']
 }
